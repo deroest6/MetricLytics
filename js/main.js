0504d1830d777df982b16bca13c1dd79bfb16be8
Jantsen DeRoest <deroest6@hotmail.com>
2017-07-12 12:56:42 -0600

    Updated Code for browser quick fixes
    Additional changes still need to be made for various devices to appear
    correctly

--- js/main.js
+++ js/main.js
@@ -29,9 +29,3 @@ $(window).scroll(
 $(window).scroll(function(){
     $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
   });
-
-/*win.scroll(function(){
-  scrollPosition = win.scrollTop();
-  scrollRatio = 1 - scrollPosition / 300;
-  $(".top").css("opacity", scrollRatio);
-*/
